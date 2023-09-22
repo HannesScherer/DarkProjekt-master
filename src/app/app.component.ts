dc4df2e348395c075f6519b49067d5d0d67f5676
DESKTOP-Q4L0QOU\Scherer <hannes.scherer@studmail.w-hs.de>
2023-09-22 09:03:37 +0200

    Zwischenstand mit dark Background  hell soon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -20,36 +20,7 @@ export class AppComponent {
   gitHubLight = './assets/github-mark-kleiner.svg';
   linkedInLight = './assets/linkedin.svg';;
   discordLight = './assets/discord-mark-black-kleiner.svg';
-
-
-  
-  
-  
-  darkmode = false;
-  modetoggle(): boolean {
-    this.darkmode = !this.darkmode;
-    document.documentElement.setAttribute('dark-theme', this.darkmode ? "dark": "light");
-    this.updateAssetPaths();
-    return this.darkmode;
-
-  }
-  updateAssetPaths() {
-    if (!this.darkmode) {
-        this.lightNav = './assets/BurgernavV4.svg';
-        this.lightLogo = './assets/LogoAntrazit.svg';
-        this.lightKreuz = './assets/closeBlack.svg';
-        this.gitHubLight = './assets/github-mark-kleiner.svg';
-        this.linkedInLight = './assets/linkedin.svg';
-        this.discordLight = './assets/discord-mark-black-kleiner.svg';
-    } else {
-        this.lightNav = './assets/BurgernavV4Dark.svg';
-        this.lightLogo = './assets/LogoGelb.svg';
-        this.lightKreuz = './assets/closeGelb.svg';
-        this.gitHubLight = './assets/github-mark-white.svg';
-        this.linkedInLight = './assets/linkedin-white.svg'; 
-        this.discordLight = './assets/discord-mark-white.svg';
-    }
-  }
+  backGround = './assets/backgroundDark.jpg';
 
   tvalue:string ='';
   getValue(val:string){
--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -8,6 +8,13 @@ import { Component } from '@angular/core';
 export class NavComponent {
   lightNav = './assets/BurgernavV4.svg';
   menuOpen = false;
+  lightLogo = './assets/LogoAntrazit.svg';;
+  lightKreuz = '';
+  gitHubLight = './assets/github-mark-kleiner.svg';
+  linkedInLight = './assets/linkedin.svg';;
+  discordLight = './assets/discord-mark-black-kleiner.svg';
+  backGround = './assets/backgroundDark.jpg';
+
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
    /*
@@ -18,5 +25,30 @@ export class NavComponent {
     }
   
   */
+  }
+  darkmode = false;
+  modetoggle(): boolean {
+    this.darkmode = !this.darkmode;
+    document.documentElement.setAttribute('dark-theme', this.darkmode ? "dark": "light");
+    this.updateAssetPaths();
+    return this.darkmode;
+
+  }
+  updateAssetPaths() {
+    if (!this.darkmode) {
+        this.lightNav = './assets/BurgernavV4.svg';
+        this.lightLogo = './assets/LogoAntrazit.svg';
+        this.lightKreuz = './assets/closeBlack.svg';
+        this.gitHubLight = './assets/github-mark-kleiner.svg';
+        this.linkedInLight = './assets/linkedin.svg';
+        this.discordLight = './assets/discord-mark-black-kleiner.svg';
+    } else {
+        this.lightNav = './assets/BurgernavV4Dark.svg';
+        this.lightLogo = './assets/LogoGelb.svg';
+        this.lightKreuz = './assets/closeGelb.svg';
+        this.gitHubLight = './assets/github-mark-white.svg';
+        this.linkedInLight = './assets/linkedin-white.svg'; 
+        this.discordLight = './assets/discord-mark-white.svg';
+    }
   }
 }
